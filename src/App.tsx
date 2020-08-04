e1ae9269214b040213d13de335478c37a07e6762
Bria Vicenti <briavicenti@curalate.com>
2020-08-03 20:08:42 -0700

    build(): fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import styled, { ThemeProvider} from 'styled-components';
+import styled, { ThemeProvider } from "styled-components";
 
 import theme from "./theme/theme";
 
 const App: React.FunctionComponent = () => {
   return (
-    <ThemeProvider theme={ theme }>
-      <Container>
-        Hello
-      </Container>
+    <ThemeProvider theme={theme}>
+      <Container>Hello Rachael</Container>
     </ThemeProvider>
   );
-}
+};
 
-const Container = styled.div(({ theme }) => `
+const Container = styled.div(
+  ({ theme }) => `
   width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${ theme.colors.base };
-  color: ${ theme.colors.primary };
-`);
+  background-color: ${theme.colors.base};
+  color: ${theme.colors.primary};
+`
+);
 
 export default App;
