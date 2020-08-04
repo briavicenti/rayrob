62b3c362355ddb2f0050a727092eee65eaf1c08b
Bria Vicenti <briavicenti@curalate.com>
2020-08-03 19:58:25 -0700

    build(): add a bunch of libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider} from 'styled-components';
 
-function App() {
+import theme from "./theme/theme";
+
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={ theme }>
+      <Container>
+        Hello
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.div(({ theme }) => `
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${ theme.colors.base };
+  color: ${ theme.colors.primary };
+`);
 
 export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: '#3b6978', // med blue
+    secondary: '#84a9ac', // light blue
+    tertiary: '#204051', // dark blue
+    body: '#1c1c1c', // dark gray
+    base: '#e7dfd5', // sand
+  },
+  fontSizes: {
+    body: '14px',
+    title: '24px',
+  },
+} as const;
